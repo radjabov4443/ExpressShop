1bc24ed2609470bdddc27316fba5975d987c0c09
Javlonbek Rajabov <radjabov377@mail.ru>
2022-03-25 16:02:02 +0500

    Add project files.

---
+++ ExpressShop.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,17 @@
+﻿using ExpressShop.Domain.Commons;
+using ExpressShop.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressShop.Domain.Entities.Orders
+{
+    public class Order : IAuditable
+    {
+        public Guid Id { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
---
+++ ExpressShop.Domain/Entities/Products/Product.cs
@@ -0,0 +1,24 @@
+﻿using ExpressShop.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressShop.Domain.Entities.Products
+{
+    public class Product : IAuditable
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+        
+        public DateTime CreatedAt { get; set; }
+
+        public int Expiration { get; set; }
+    }
+}
